7c6c02e5b6a7ce9bc9f5bc03f63ea2526dcf5e72
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2022-08-05 17:49:43 -0400

    Add files via upload

--- Streamlit/NFAdashboard.py
+++ Streamlit/NFAdashboard.py
@@ -2,21 +2,22 @@ import streamlit as st # web development
 import yfinance as yf
 import pandas as pd
 
-st.set_page_config(
-    page_title="NotFinancialAdvice",
-    page_icon="📊",
-    layout= "wide"    
-)
 
 # dashboard title
 st.title("NotFinanialAdvice Streamlit Finance Dashboard")
 
+# Tickers from the Dow 30 from YFinance
 tickers = ('AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CSCO', 'CVX', 'GS',	'HD', 'HON', 'IBM', 'INTC', 'JNJ', 'KO', 'JPM', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 'PG', 'TRV', 'UNH','CRM', 'VZ', 'V', 'WBA', 'WMT', 'DIS', 'DOW')
+
+# Takes the tickers ans puts them in a list to be selected in a dropdown
 dropdown = st.multiselect('Pick your assets', tickers)
 
+# Start time
 start = st.date_input('Start', value = pd.to_datetime('2021-01-01'))
+# End time
 end = st.date_input('End', value = pd.to_datetime('today'))
 
+# Relative return of the users deseired stock.
 def relativereturn(df):
     rel = df.pct_change()
     cumulativereturn = (1-rel).cumprod() -1
@@ -24,9 +25,59 @@ def relativereturn(df):
     return cumulativereturn
 
 
-
+# Starts the drop down at 0 to remove an initial error 
 if len(dropdown) > 0:
 #     df = yf.download(dropdown,start,end)['Adj Close']
     df = relativereturn(yf.download(dropdown,start,end)['Adj Close'])
     st.header('Returns of {}'.format(dropdown))
     st.line_chart(df)
+    
+fundamental_list_df = ('zip', 'sector', 'fullTimeEmployees', 'longBusinessSummary', 
+                       'city', 'phone', 'state', 'country', 'companyOfficers', 
+                       'website', 'maxAge', 'address1', 'fax', 'industry', 
+                       'ebitdaMargins', 'profitMargins', 'grossMargins', 
+                       'operatingCashflow', 'revenueGrowth', 'operatingMargins', 
+                       'ebitda', 'targetLowPrice', 'recommendationKey', 
+                       'grossProfits', 'freeCashflow', 'targetMedianPrice', 
+                       'currentPrice', 'earningsGrowth', 'currentRatio', 'returnOnAssets', 
+                       'numberOfAnalystOpinions', 'targetMeanPrice', 'debtToEquity', 
+                       'returnOnEquity', 'targetHighPrice', 'totalCash', 'totalDebt', 
+                       'totalRevenue', 'totalCashPerShare', 'financialCurrency', 
+                       'revenuePerShare', 'quickRatio', 'recommendationMean', 'exchange', 
+                       'shortName', 'longName', 'exchangeTimezoneName', 
+                       'exchangeTimezoneShortName', 'isEsgPopulated', 'gmtOffSetMilliseconds', 
+                       'quoteType', 'symbol', 'messageBoardId', 'market', 'annualHoldingsTurnover', 
+                       'enterpriseToRevenue', 'beta3Year', 'enterpriseToEbitda', '52WeekChange', 
+                       'morningStarRiskRating', 'forwardEps', 'revenueQuarterlyGrowth', 
+                       'sharesOutstanding', 'fundInceptionDate', 'annualReportExpenseRatio', 
+                       'totalAssets', 'bookValue', 'sharesShort', 'sharesPercentSharesOut', 
+                       'fundFamily', 'lastFiscalYearEnd', 'heldPercentInstitutions', 
+                       'netIncomeToCommon', 'trailingEps', 'lastDividendValue', 
+                       'SandP52WeekChange', 'priceToBook', 'heldPercentInsiders', 
+                       'nextFiscalYearEnd', 'yield', 'mostRecentQuarter', 'shortRatio', 
+                       'sharesShortPreviousMonthDate', 'floatShares', 'beta', 'enterpriseValue', 
+                       'priceHint', 'threeYearAverageReturn', 'lastSplitDate', 'lastSplitFactor', 
+                       'legalType', 'lastDividendDate', 'morningStarOverallRating', 
+                       'earningsQuarterlyGrowth', 'priceToSalesTrailing12Months', 'dateShortInterest', 
+                       'pegRatio', 'ytdReturn', 'forwardPE', 'lastCapGain', 'shortPercentOfFloat', 
+                       'sharesShortPriorMonth', 'impliedSharesOutstanding', 'category', 
+                       'fiveYearAverageReturn', 'previousClose', 'regularMarketOpen', 
+                       'twoHundredDayAverage', 'trailingAnnualDividendYield', 'payoutRatio', 
+                       'volume24Hr', 'regularMarketDayHigh', 'navPrice', 'averageDailyVolume10Day',  
+                       
+                       
+                       'regularMarketPreviousClose', 'fiftyDayAverage', 'trailingAnnualDividendRate', 
+                       'open', 'toCurrency', 'averageVolume10days', 'expireDate', 'algorithm', 
+                       'dividendRate', 'exDividendDate', 'circulatingSupply', 'startDate', 
+                       'regularMarketDayLow', 'currency', 'trailingPE', 'regularMarketVolume', 
+                       'lastMarket', 'maxSupply', 'openInterest', 'marketCap', 'volumeAllCurrencies', 
+                       'strikePrice', 'averageVolume', 'dayLow', 'ask', 'askSize', 'volume', 
+                       'fiftyTwoWeekHigh', 'fromCurrency', 'fiveYearAvgDividendYield', 
+                       'fiftyTwoWeekLow', 'bid', 'tradeable', 'dividendYield', 'bidSize', 'dayHigh',
+                       'coinMarketCapLink', 'regularMarketPrice', 'preMarketPrice', 'logo_url', 
+                       'trailingPegRatio')
+
+
+fundamental_dropdown = st.multiselect('Pick your fundamental interest', fundamental_list_df)
+
+st.write(dropdown.fundamental_list_df)
--- Streamlit/pages/Prophet.py
+++ Streamlit/pages/Prophet.py
@@ -23,25 +23,40 @@ end = st.date_input('End', value = pd.to_datetime('today'))
 
 df = yf.download(dropdown,start,end).Close
 
-# Create a Prophet model for Forex data
+# Create a Prophet model for model_dow
 model_dow = Prophet(yearly_seasonality=True)
-
-# format dow data model for model
+# format 'dow_prophet_model' to fit the prophet functions parameters. 
 dow_prophet_model = df.reset_index()
 dow_prophet_model.columns = ['ds', 'y']
-
 # Fit the Prophet model for dow data
 model_dow.fit(dow_prophet_model)
-
 # Forecast one year of weekly future trends data for the Future dow Closing Prices 
 future_dow = model_dow.make_future_dataframe(periods=52, freq="W")
-
 # Make predictions for forecast_dow using the future_dow DataFrame
 forecast_dow = model_dow.predict(future_dow)
-
 # Plot predictions for our forecast_dow DataFrame for the 52 week period 
 forecast_dow_predictions = forecast_dow[['yhat', 'yhat_lower', 'yhat_upper']].iloc[-52:,:]
 
+# Use the plot_components function to visualize the forecast results 
+figures = model_dow.plot_components(forecast_dow)
+
+# Create a `forecast_future_month` Dataframe, which contains just forecasts for the future
+# The DataFrame should include the columns yhat_upper, yhat_lower, and yhat
+
+end = pd.to_datetime('today').strftime('%Y-%m-%d')
+time_delta = dt.timedelta(days = 20)
+start = (pd.to_datetime(end) - time_delta).strftime('%Y-%m-%d')
+
+forecast_future_month = forecast_dow.loc[start:end][["yhat_upper", "yhat_lower", "yhat"]]
+
+# Replace the column names to something less technical sounding
+forecast_future_month = forecast_future_month.rename(
+    columns={
+        "yhat_upper": "Best Case",
+        "yhat_lower": "Worst Case", 
+        "yhat": "Most Likely Case"
+    }
+)
 
 
 # Dashboard 
@@ -53,3 +68,11 @@ fig, ax = plt.subplots()
 ax.plot(forecast_dow_predictions)
 ax.legend(['yhat', 'yhat_lower', 'yhat_upper'])
 st.pyplot(fig)
+
+st.pyplot(figures)
+
+# Review the last five rows of the DataFrame
+st.write(forecast_future_month.tail())
+
+# Display the average forecasted price 
+st.write(forecast_future_month.mean())
