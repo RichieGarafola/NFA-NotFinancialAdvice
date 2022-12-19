675d500195254925d407975a13a758ae567d2ab4
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2022-12-19 11:05:10 -0500

    Update 05_Technical Analysis.py

--- pages/05_Technical Analysis.py
+++ pages/05_Technical Analysis.py
@@ -27,7 +27,8 @@ tickers = (dow + sp500)
 
 tickers_dropdown = st.selectbox('Choose a stock ticker', tickers)
 
-ohlc = web.DataReader(tickers_dropdown, 'yahoo') #, start='2019-09-10', end='2019-10-09')
+# ohlc = web.DataReader(tickers_dropdown, 'yahoo') #, start='2019-09-10', end='2019-10-09')
+ohlc = yf.download(tickers_dropdown)
 ohlc = ohlc.rename(columns={'High':'high', 'Low':'low', 'Open':'open', 'Close':'close', 'Volume':'volume', 'Adj Close':'adj close'})
 ohlc = ohlc.sort_index(ascending=False)
 
