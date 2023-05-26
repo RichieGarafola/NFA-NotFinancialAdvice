15af28eb9e1fbadda3a336c7415fa74b2aae359d
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2023-05-26 06:47:28 -0400

    Add files via upload

---
+++ pages/07_Get_in_touch.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.header(":mailbox: Get In Touch With Me!")
+
+contact_form = """
+<form action="https://formsubmit.co/8c1144f613c50b43e7ddf63b49e40672" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="your name" required>
+    <input type="email" name="email" placeholder="your email" required>
+    <textarea name="message" placeholder="Leave your comments"></textarea>
+    <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html = True)
+# Use local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        
+local_css("Desktop/Github/Project3/Project3/Richie/Streamlit/pages/style/style.css")
