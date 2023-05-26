727e9c8f609969526f0ee1212666735b5a910d78
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2023-05-26 06:50:49 -0400

    Update 07_Get_in_touch.py

--- pages/07_Get_in_touch.py
+++ pages/07_Get_in_touch.py
@@ -18,4 +18,4 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
         
-local_css("Desktop/Github/Project3/Project3/Richie/Streamlit/pages/style/style.css")
+local_css("./Streamlit/pages/style/style.css")
