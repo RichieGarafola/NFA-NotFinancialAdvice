090be1f504e483fdc3b7b9e7a1d31b4dc880b82c
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2023-05-26 21:00:08 -0400

    Add files via upload

---
+++ pages/03_Sentiment_Analysis_From_FinViz.py
@@ -0,0 +1,154 @@
+# Dashboard
+import streamlit as st
+
+# get data
+from urllib.request import urlopen, Request
+
+# parse data from FinViz 
+from bs4 import BeautifulSoup
+import os
+
+# manipulate and store the data in DataFrames
+import pandas as pd
+
+# plot the sentiment on a chart
+import matplotlib.pyplot as plt
+
+# NLTK VADER for sentiment analysis on the news headlines
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+
+# wordcloud and stop words
+from wordcloud import WordCloud, STOPWORDS
+
+
+# take the raw part of the url, we will append the ticker to the end of the link to pull up its data
+# Extract data from finviz use the raw url.
+finwiz_url = 'https://finviz.com/quote.ashx?t='
+
+# Tickers from the Dow 30 from YFinance
+dow = 'AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CSCO', 'CVX', 'GS', 'HD', 'HON', 'IBM', 'INTC', 'JNJ', 'KO', 'JPM', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 'PG', 'TRV', 'UNH','CRM', 'VZ', 'V', 'WBA', 'WMT', 'DIS', 'DOW'
+
+# Tickers from the sp500 from YFinance
+sp500 = 'MMM', 'AOS', 'ABT', 'ABBV', 'ABMD', 'ACN', 'ATVI', 'ADM', 'ADBE', 'AAP', 'AMD', 'AES', 'AFL', 'A', 'APD', 'AKAM', 'ALK', 'ALB', 'ARE', 'ALGN', 'ALLE', 'LNT', 'ALL', 'GOOGL', 'GOOG', 'MO', 'AMZN', 'AMCR', 'AEE', 'AAL', 'AEP', 'AXP', 'AIG', 'AMT', 'AWK', 'AMP', 'ABC', 'AME', 'AMGN', 'APH', 'ADI', 'ANSS', 'ANTM', 'AON', 'APA', 'AAPL', 'AMAT', 'APTV', 'ANET', 'AJG', 'AIZ', 'T', 'ATO', 'ADSK', 'ADP', 'AZO', 'AVB', 'AVY', 'BKR', 'BLL', 'BAC', 'BBWI', 'BAX', 'BDX', 'BRK.B', 'BBY', 'BIO', 'TECH', 'BIIB', 'BLK', 'BK', 'BA', 'BKNG', 'BWA', 'BXP', 'BSX', 'BMY', 'AVGO', 'BR', 'BRO', 'BF.B', 'CHRW', 'CDNS', 'CZR', 'CPB', 'COF', 'CAH', 'KMX', 'CCL', 'CARR', 'CTLT', 'CAT', 'CBOE', 'CBRE', 'CDW', 'CE', 'CNC', 'CNP', 'CDAY', 'CERN', 'CF', 'CRL', 'SCHW', 'CHTR', 'CVX', 'CMG', 'CB', 'CHD', 'CI', 'CINF', 'CTAS', 'CSCO', 'C', 'CFG', 'CTXS', 'CLX', 'CME', 'CMS', 'KO', 'CTSH', 'CL', 'CMCSA', 'CMA', 'CAG', 'COP', 'ED', 'STZ', 'CPRT'  'GLW', 'CTVA', 'COST', 'CTRA', 'CCI', 'CSX', 'CMI', 'CVS', 'DHI', 'DHR', 'DRI', 'DVA', 'DE', 'DAL', 'XRAY', 'DVN', 'DXCM', 'FANG', 'DLR', 'DFS', 'DISCA', 'DISCK', 'DISH', 'DG', 'DLTR', 'D', 'DPZ', 'DOV', 'DOW', 'DTE', 'DUK', 'DRE', 'DD', 'DXC', 'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA', 'LLY', 'EMR', 'ENPH', 'ETR', 'EOG', 'EFX', 'EQIX', 'EQR', 'ESS', 'EL', 'ETSY', 'RE', 'EVRG', 'ES', 'EXC', 'EXPE', 'EXPD', 'EXR', 'XOM', 'FFIV', 'FB', 'FAST', 'FRT', 'FDX', 'FIS', 'FITB', 'FRC', 'FE', 'FISV', 'FLT', 'FMC', 'F', 'FTNT', 'FTV',  'FBHS', 'FOXA', 'FOX', 'BEN', 'FCX', 'GPS', 'GRMN', 'IT', 'GNRC', 'GD', 'GE', 'GIS', 'GM', 'GPC', 'GILD', 'GPN', 'GL', 'GS', 'HAL', 'HBI', 'HAS', 'HCA', 'PEAK', 'HSIC', 'HES', 'HPE', 'HLT', 'HOLX', 'HD', 'HON', 'HRL', 'HST', 'HWM', 'HPQ', 'HUM', 'HBAN', 'HII', 'IBM', 'IEX', 'IDXX', 'INFO', 'ITW', 'ILMN', 'INCY', 'IR', 'INTC', 'ICE', 'IFF', 'IP', 'IPG', 'INTU', 'ISRG', 'IVZ', 'IPGP', 'IQV', 'IRM', 'JBHT', 'JKHY', 'J', 'SJM', 'JNJ', 'JCI', 'JPM', 'JNPR', 'KSU',  'K', 'KEY', 'KEYS', 'KMB', 'KIM', 'KMI', 'KLAC', 'KHC', 'KR', 'LHX', 'LH', 'LRCX', 'LW', 'LVS', 'LEG', 'LDOS', 'LEN', 'LNC', 'LIN', 'LYV', 'LKQ', 'LMT', 'L', 'LOW', 'LUMN', 'LYB', 'MTB', 'MRO', 'MPC', 'MKTX', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MTCH', 'MKC', 'MCD', 'MCK', 'MDT', 'MRK', 'MET', 'MTD', 'MGM', 'MCHP', 'MU', 'MSFT', 'MAA', 'MRNA', 'MHK', 'TAP', 'MDLZ', 'MPWR', 'MNST', 'MCO', 'MS', 'MSI', 'MSCI', 'NDAQ', 'NTAP', 'NFLX', 'NWL', 'NEM', 'NWSA', 'NWS', 'NEE', 'NLSN', 'NKE', 'NI', 'NSC', 'NTRS', 'NOC', 'NLOK', 'NCLH', 'NRG', 'NUE', 'NVDA', 'NVR', 'NXPI', 'ORLY', 'OXY', 'ODFL', 'OMC', 'OKE', 'ORCL', 'OGN', 'OTIS', 'PCAR', 'PKG', 'PH', 'PAYX', 'PAYC', 'PYPL', 'PENN', 'PNR', 'PBCT', 'PEP', 'PKI', 'PFE', 'PM', 'PSX', 'PNW', 'PXD', 'PNC', 'POOL', 'PPG', 'PPL', 'PFG', 'PG', 'PGR', 'PLD', 'PRU', 'PTC', 'PEG', 'PSA', 'PHM', 'PVH', 'QRVO', 'QCOM', 'PWR', 'DGX', 'RL', 'RJF',  'RTX', 'O', 'REG', 'REGN', 'RF',  'RSG', 'RMD', 'RHI', 'ROK', 'ROL', 'ROP', 'ROST', 'RCL', 'SPGI', 'CRM', 'SBAC', 'SLB', 'STX', 'SEE', 'SRE', 'NOW', 'SHW', 'SPG', 'SWKS', 'SNA', 'SO', 'LUV', 'SWK', 'SBUX', 'STT', 'STE', 'SYK', 'SIVB', 'SYF', 'SNPS', 'SYY', 'TMUS', 'TROW', 'TTWO', 'TPR', 'TGT', 'TEL', 'TDY', 'TFX', 'TER', 'TSLA', 'TXN', 'TXT', 'COO', 'HIG', 'HSY', 'MOS', 'TRV', 'DIS', 'TMO', 'TJX', 'TSCO', 'TT', 'TDG', 'TRMB', 'TFC', 'TWTR', 'TYL', 'TSN', 'USB', 'UDR', 'ULTA', 'UAA', 'UA', 'UNP', 'UAL', 'UPS', 'URI', 'UNH', 'UHS', 'VLO', 'VTR', 'VRSN', 'VRSK', 'VZ', 'VRTX', 'VFC', 'VIAC', 'VTRS', 'V', 'VNO', 'VMC', 'WRB', 'GWW', 'WAB', 'WBA', 'WMT', 'WM', 'WAT', 'WEC', 'WFC', 'WELL', 'WST', 'WDC', 'WU', 'WRK', 'WY', 'WHR', 'WMB', 'WLTW', 'WYNN', 'XEL', 'XLNX', 'XYL', 'YUM', 'ZBRA', 'ZBH', 'ZION', 'ZTS'
+
+tickers = (dow + sp500)
+tickers_dropdown = st.selectbox('Choose a stock ticker', tickers)
+
+# Initiate an empty dictionary to hold the news tables from website
+news_tables = {}
+tickers = [tickers_dropdown]
+
+for ticker in tickers:
+    url = finwiz_url + ticker
+    req = Request(url=url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}) 
+    response = urlopen(req)    
+    # Read the contents of the file into 'html'
+    html = BeautifulSoup(response)
+    # Find 'news-table' in the Soup and load it into 'news_table'
+    news_table = html.find(id='news-table')
+    # Add the table to our dictionary
+    news_tables[ticker] = news_table
+
+    
+# <td> tag defines the standard cells in the table which are displayed as normal-weight, left-aligned text. 
+# <tr> tag defines the table rows.
+
+# Read headlines for 'stock' 
+stock = news_tables[tickers_dropdown]
+# Get all the table rows tagged in HTML with <tr> into 'stock_tr'
+stock_tr = stock.findAll('tr')
+
+#Create a list to append the titles
+title_list = []
+        
+# The enumerate() function takes a collection and returns it as an enumerate object.
+for index, table_row in enumerate(stock_tr):
+    # Read the text of the element 'a' (Anchor tag) into 'title' 
+    # title = table_row.a.text
+    title = table_row.a.text if table_row.a else ""
+    # Read the text of the element 'td' into 'timestamp' for the timestamp
+    timestamp = table_row.td.text.split()
+    
+    # if the length of 'timestamp' is 1, load 'time' as the only element
+    if len(timestamp) == 1:
+        time = timestamp[0]
+            
+    # else load 'date' as the 1st element and 'time' as the second    
+    else:
+        date = timestamp[0]
+        time = timestamp[1]
+    ticker = tickers
+            
+    # Append the title and timestamp to list format. 
+    title_list.append([tickers, date, time, title])
+    # Create a dataframe using the 'title_list'
+    finviz_headlines = pd.DataFrame(title_list, columns=['ticker', 'date', 'time', 'title'])
+    
+st.subheader("Most recent 100 Headlines")    
+st.write(finviz_headlines)
+    
+# Sentiment calculation based on compound score
+def get_sentiment(score):
+    """
+    Calculates the sentiment based on the compound score.
+    """
+    result = 0  # Neutral by default
+    if score >= 0.05:  # Positive
+        result = 1
+    elif score <= -0.05:  # Negative
+        result = -1
+
+    return result
+
+
+# Create the sentiment scores DataFrame
+analyzer = SentimentIntensityAnalyzer()
+
+title_sent = {
+    "title_compound": [],
+    "title_pos": [],
+    "title_neu": [],
+    "title_neg": [],
+    "title_sent": [],
+}
+
+
+# Get sentiment for the title
+for index, row in finviz_headlines.iterrows():
+    try:
+        # Sentiment scoring with VADER
+        title_sentiment = analyzer.polarity_scores(row["title"])
+        title_sent["title_compound"].append(title_sentiment["compound"])
+        title_sent["title_pos"].append(title_sentiment["pos"])
+        title_sent["title_neu"].append(title_sentiment["neu"])
+        title_sent["title_neg"].append(title_sentiment["neg"])
+        title_sent["title_sent"].append(get_sentiment(title_sentiment["compound"]))
+    except AttributeError:
+        pass
+
+
+# Attaching sentiment columns to the News DataFrame
+title_sentiment_df = pd.DataFrame(title_sent)
+finviz_headlines = finviz_headlines.join(title_sentiment_df)
+
+st.subheader("Sentiment Statistics")  
+st.write(finviz_headlines.describe())
+
+###
+# ADD PIE CHART FROM TWITTER!!
+###
+
+def word_cloud(text):
+    stopwords = set(STOPWORDS)
+    allWords = ' '.join([title for title in text])
+    wordCloud = WordCloud(background_color='black',width = 1600, height = 800,stopwords = stopwords,min_font_size = 20,max_font_size=150,colormap='prism').generate(allWords)
+    fig, ax = plt.subplots(figsize=(20,10), facecolor='k')
+    plt.imshow(wordCloud)
+    ax.axis("off")
+    fig.tight_layout(pad=0)
+    plt.show()
+    st.pyplot(plt)
+st.subheader("Wordcloud for further analysis")  
+word_cloud(finviz_headlines['title'].values)
