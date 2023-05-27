b0cf6d8ed1a88c74f710a253f6093bb9db0f97e5
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2023-05-26 20:49:51 -0400

    Add files via upload

--- pages/01_Prophet.py
+++ pages/01_Prophet.py
@@ -11,7 +11,7 @@ from prophet import Prophet
 # Pull in financial information
 import yfinance as yf
 
-
+# Page configuration and title
 st.set_page_config(
     page_title="NotFinancialAdvice - Prophet",
     page_icon="📊",
@@ -30,7 +30,6 @@ dow = 'AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CSCO', 'CVX', 'GS', 'HD', 'HON', 'IBM
 sp500 = 'MMM', 'AOS', 'ABT', 'ABBV', 'ABMD', 'ACN', 'ATVI', 'ADM', 'ADBE', 'AAP', 'AMD', 'AES', 'AFL', 'A', 'APD', 'AKAM', 'ALK', 'ALB', 'ARE', 'ALGN', 'ALLE', 'LNT', 'ALL', 'GOOGL', 'GOOG', 'MO', 'AMZN', 'AMCR', 'AEE', 'AAL', 'AEP', 'AXP', 'AIG', 'AMT', 'AWK', 'AMP', 'ABC', 'AME', 'AMGN', 'APH', 'ADI', 'ANSS', 'ANTM', 'AON', 'APA', 'AAPL', 'AMAT', 'APTV', 'ANET', 'AJG', 'AIZ', 'T', 'ATO', 'ADSK', 'ADP', 'AZO', 'AVB', 'AVY', 'BKR', 'BLL', 'BAC', 'BBWI', 'BAX', 'BDX', 'BRK.B', 'BBY', 'BIO', 'TECH', 'BIIB', 'BLK', 'BK', 'BA', 'BKNG', 'BWA', 'BXP', 'BSX', 'BMY', 'AVGO', 'BR', 'BRO', 'BF.B', 'CHRW', 'CDNS', 'CZR', 'CPB', 'COF', 'CAH', 'KMX', 'CCL', 'CARR', 'CTLT', 'CAT', 'CBOE', 'CBRE', 'CDW', 'CE', 'CNC', 'CNP', 'CDAY', 'CERN', 'CF', 'CRL', 'SCHW', 'CHTR', 'CVX', 'CMG', 'CB', 'CHD', 'CI', 'CINF', 'CTAS', 'CSCO', 'C', 'CFG', 'CTXS', 'CLX', 'CME', 'CMS', 'KO', 'CTSH', 'CL', 'CMCSA', 'CMA', 'CAG', 'COP', 'ED', 'STZ', 'CPRT'  'GLW', 'CTVA', 'COST', 'CTRA', 'CCI', 'CSX', 'CMI', 'CVS', 'DHI', 'DHR', 'DRI', 'DVA', 'DE', 'DAL', 'XRAY', 'DVN', 'DXCM', 'FANG', 'DLR', 'DFS', 'DISCA', 'DISCK', 'DISH', 'DG', 'DLTR', 'D', 'DPZ', 'DOV', 'DOW', 'DTE', 'DUK', 'DRE', 'DD', 'DXC', 'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA', 'LLY', 'EMR', 'ENPH', 'ETR', 'EOG', 'EFX', 'EQIX', 'EQR', 'ESS', 'EL', 'ETSY', 'RE', 'EVRG', 'ES', 'EXC', 'EXPE', 'EXPD', 'EXR', 'XOM', 'FFIV', 'FB', 'FAST', 'FRT', 'FDX', 'FIS', 'FITB', 'FRC', 'FE', 'FISV', 'FLT', 'FMC', 'F', 'FTNT', 'FTV',  'FBHS', 'FOXA', 'FOX', 'BEN', 'FCX', 'GPS', 'GRMN', 'IT', 'GNRC', 'GD', 'GE', 'GIS', 'GM', 'GPC', 'GILD', 'GPN', 'GL', 'GS', 'HAL', 'HBI', 'HAS', 'HCA', 'PEAK', 'HSIC', 'HES', 'HPE', 'HLT', 'HOLX', 'HD', 'HON', 'HRL', 'HST', 'HWM', 'HPQ', 'HUM', 'HBAN', 'HII', 'IBM', 'IEX', 'IDXX', 'INFO', 'ITW', 'ILMN', 'INCY', 'IR', 'INTC', 'ICE', 'IFF', 'IP', 'IPG', 'INTU', 'ISRG', 'IVZ', 'IPGP', 'IQV', 'IRM', 'JBHT', 'JKHY', 'J', 'SJM', 'JNJ', 'JCI', 'JPM', 'JNPR', 'KSU',  'K', 'KEY', 'KEYS', 'KMB', 'KIM', 'KMI', 'KLAC', 'KHC', 'KR', 'LHX', 'LH', 'LRCX', 'LW', 'LVS', 'LEG', 'LDOS', 'LEN', 'LNC', 'LIN', 'LYV', 'LKQ', 'LMT', 'L', 'LOW', 'LUMN', 'LYB', 'MTB', 'MRO', 'MPC', 'MKTX', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MTCH', 'MKC', 'MCD', 'MCK', 'MDT', 'MRK', 'MET', 'MTD', 'MGM', 'MCHP', 'MU', 'MSFT', 'MAA', 'MRNA', 'MHK', 'TAP', 'MDLZ', 'MPWR', 'MNST', 'MCO', 'MS', 'MSI', 'MSCI', 'NDAQ', 'NTAP', 'NFLX', 'NWL', 'NEM', 'NWSA', 'NWS', 'NEE', 'NLSN', 'NKE', 'NI', 'NSC', 'NTRS', 'NOC', 'NLOK', 'NCLH', 'NRG', 'NUE', 'NVDA', 'NVR', 'NXPI', 'ORLY', 'OXY', 'ODFL', 'OMC', 'OKE', 'ORCL', 'OGN', 'OTIS', 'PCAR', 'PKG', 'PH', 'PAYX', 'PAYC', 'PYPL', 'PENN', 'PNR', 'PBCT', 'PEP', 'PKI', 'PFE', 'PM', 'PSX', 'PNW', 'PXD', 'PNC', 'POOL', 'PPG', 'PPL', 'PFG', 'PG', 'PGR', 'PLD', 'PRU', 'PTC', 'PEG', 'PSA', 'PHM', 'PVH', 'QRVO', 'QCOM', 'PWR', 'DGX', 'RL', 'RJF',  'RTX', 'O', 'REG', 'REGN', 'RF',  'RSG', 'RMD', 'RHI', 'ROK', 'ROL', 'ROP', 'ROST', 'RCL', 'SPGI', 'CRM', 'SBAC', 'SLB', 'STX', 'SEE', 'SRE', 'NOW', 'SHW', 'SPG', 'SWKS', 'SNA', 'SO', 'LUV', 'SWK', 'SBUX', 'STT', 'STE', 'SYK', 'SIVB', 'SYF', 'SNPS', 'SYY', 'TMUS', 'TROW', 'TTWO', 'TPR', 'TGT', 'TEL', 'TDY', 'TFX', 'TER', 'TSLA', 'TXN', 'TXT', 'COO', 'HIG', 'HSY', 'MOS', 'TRV', 'DIS', 'TMO', 'TJX', 'TSCO', 'TT', 'TDG', 'TRMB', 'TFC', 'TWTR', 'TYL', 'TSN', 'USB', 'UDR', 'ULTA', 'UAA', 'UA', 'UNP', 'UAL', 'UPS', 'URI', 'UNH', 'UHS', 'VLO', 'VTR', 'VRSN', 'VRSK', 'VZ', 'VRTX', 'VFC', 'VIAC', 'VTRS', 'V', 'VNO', 'VMC', 'WRB', 'GWW', 'WAB', 'WBA', 'WMT', 'WM', 'WAT', 'WEC', 'WFC', 'WELL', 'WST', 'WDC', 'WU', 'WRK', 'WY', 'WHR', 'WMB', 'WLTW', 'WYNN', 'XEL', 'XLNX', 'XYL', 'YUM', 'ZBRA', 'ZBH', 'ZION', 'ZTS'
 
 tickers = (dow + sp500)
-
 dropdown = st.sidebar.selectbox('Pick your asset', tickers)
 
 start = st.sidebar.date_input('Start', value = pd.to_datetime('2021-01-01'))
@@ -41,9 +40,8 @@ df = yf.download(dropdown,start,end).Close
 # Create a Prophet model for model_dow
 model_dow = Prophet(yearly_seasonality=True)
 
-# format 'dow_prophet_model' to fit the prophet functions parameters. 
-dow_prophet_model = df.reset_index()
-dow_prophet_model.columns = ['ds', 'y']
+# Format 'dow_prophet_model' to fit the prophet functions parameters
+dow_prophet_model = df.reset_index().rename(columns={'Date': 'ds', 'Close': 'y'})
 
 # Fit the Prophet model for dow data
 model_dow.fit(dow_prophet_model)
@@ -60,10 +58,6 @@ forecast_dow_predictions = forecast_dow[['yhat', 'yhat_lower', 'yhat_upper']].il
 
 # Use the plot_components function to visualize the forecast results 
 figures = model_dow.plot_components(forecast_dow_figures)
-
-# # Set the datetime index of the forecast_dow data, using the ds column
-# forecast_dow_index = forecast_dow.set_index("ds")
-
 
 start = pd.to_datetime('today').strftime('%Y-%m-%d')
 time_delta = dt.timedelta(days = 20)
@@ -84,42 +78,24 @@ forecast_future_month = forecast_future_month.rename(
 # Dashboard 
 
 ############################################
-# plot 1 not sccaled 
-# st.title("Prophet Forecast")
-# st.pyplot(model_dow.plot(forecast_dow_figures))
-
-# plot 2 not scaled
-# st.title(f"{dropdown} Prophet Forecast Predictions")
-# fig, ax = plt.subplots()
-# ax.plot(forecast_dow_predictions)
-# ax.legend(['yhat', 'yhat_lower', 'yhat_upper'])
-# st.pyplot(fig)
-
-# plot 3 not scaled 
-# st.pyplot(figures)
 
 st.header(f"{dropdown} Prophet Forecast Predictions")
 fig, ax = plt.subplots()
 ax.plot(forecast_dow_predictions)
 ax.legend(['yhat', 'yhat_lower', 'yhat_upper'])
 
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.subheader(f"{dropdown} Prophet Forecast")
+    st.pyplot(model_dow.plot(forecast_dow_figures))
 
-st.subheader(f"{dropdown} Prophet Forecast")
-st.pyplot(model_dow.plot(forecast_dow_figures))
+with col2:
+    st.subheader(f"{dropdown} Prophet Forecast Statistics")
+    st.pyplot(fig)
 
-st.subheader(f"{dropdown} Prophet Forecast Statistics")
-st.pyplot(fig)
-
-st.subheader("Prophet Trends")
-st.pyplot(figures)
+with col3:
+    st.subheader("Prophet Trends")
+    st.pyplot(figures)
 
 st.subheader("21 Day Forecast")
 st.write(forecast_future_month)
-
-
-# # Review the last five rows of the DataFrame
-# st.write("The last 5 days",forecast_future_month.tail())
-
-# # Display the average forecasted price 
-# st.write("Average Forecasted price for the last 20 days", forecast_future_month.mean())
-
